a7b82877cfcbeaf81e215961130bf867dd259f25
Joshua Striano <jct3er@virginia.edu>
2025-10-29 14:42:18 -0400

    Finished 3

--- fit1c.py
+++ fit1c.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 
 def NegLogLik(hist, fit_func):
     count  = 0
-    for i in range(1, hist.GetNbinX()+1):
+    for i in range(1, hist.GetNbinsX()+1):
         xi = hist.GetBinCenter(i)
         lamda = fit_func.Eval(xi)
         yi = hist.GetBinContent(i)
@@ -34,13 +34,13 @@ nll_real = NegLogLik(h25,gaus)
 
 print(f"The NLL value is {nll_real:.3f}")
 
-exp_hist = r.TH1F("exp", "exp", h25.GetNbinX(), h25.FindFirstBinAbove(0.), h25.FindLastBinAbove(0.))
+exp_hist = r.TH1F("exp", "exp", h25.GetNbinsX(), 0, 100)
 gaus_exp=r.TF1("gaus_exp","[0]*exp(-(x-[1])*(x-[1])/[2]/[2])",0,100)
 
 for i in range(1000):
     exp_hist.Reset()
 
-    for j in range(h25.GetEntries()):
+    for j in range(25):
         exp_hist.Fill(generator.Gaus(50,10))
 
     gaus_exp.SetParameter(0,exp_hist.GetMaximum())
@@ -59,16 +59,36 @@ line.SetLineColor(r.kRed)
 line.SetLineWidth(2)
 line.Draw("same")
 tc.Update()
-#tc.SaveAs("result3.pdf")
+tc.SaveAs("result3.pdf")
 
 
 
 count = 0
-for i in range(1, hist.GetNbinX()+1):
-    if hist.GetBinCenter(i) + 1 <= nll_real:
-        count += hist.GetBinContent(i)
+for i in range(1, nll_hist.GetNbinsX()+1):
+    if nll_hist.GetBinCenter(i) + 1 <= nll_real:
+        count += nll_hist.GetBinContent(i)
 
-print(f"The NLL value is {count/1000:.3f}")
+print(f"The P value is {count/1000:.3f}")
+
+
+means = np.linspace(-4.25,4.25)+mean
+nlls = []
+gaus_cont=r.TF1("gaus_cont","[0]*exp(-(x-[1])*(x-[1])/[2]/[2])",0,100)
+gaus_cont.SetParameter(0, amplitude)
+gaus_cont.SetParameter(2, sigma)
+for i in means:
+    gaus_cont.SetParameter(1, i)
+    nlls.append(NegLogLik(h25, gaus_cont))
+
+fig = plt.figure()
+plt.plot(means, nlls)
+plt.xlabel("Mean Values")
+plt.ylabel("NLL Values")
+plt.grid()
+plt.show()
+
+
+
 
     
 
